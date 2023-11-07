68e5f340787b69c4f57f228984ebb5337d39656d
martin <vazquezmartin1240@gmail.com>
2023-11-07 03:13:44 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import Header from './Components/Header/index.jsx';
 import LogoPrincipal from './pages/Home.jsx';
+import Producto from './pages/Producto.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
+      
       <Header/>
 
       <Routes >
 
         <Route path='/' element={<LogoPrincipal/>}/>
+        <Route path='/Producto/:id' element={<Producto />} />
 
       </Routes>
 
